9ca2f7e1a3936e2cb9c28da37771427de786b16c
Scorps0530 <blueandgreen0530@gmail.com>
2020-11-17 19:22:52 +0900

    1st Commit

---
+++ ShootingClient/Assets/#Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    void Awake() { if (!instance) instance = this; }
+
+    public GameObject player;
+    public GameObject bullet;
+    public Transform firepoint;
+
+    public float pSpeed;
+    public float bSpeed;
+    public float bDestoryTime;
+
+    Vector3 MousePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        FireBullet();
+    }
+
+    private void Move()
+    {
+        MousePosition = Input.mousePosition;
+        MousePosition = Camera.main.ScreenToWorldPoint(MousePosition);
+
+        player.transform.position = Vector2.MoveTowards(player.transform.position, MousePosition, pSpeed);
+        Vector2 v2 = MousePosition - player.transform.position;
+        player.transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(v2.y, v2.x) * Mathf.Rad2Deg);
+    }
+
+    private void FireBullet()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bullet, firepoint.position, firepoint.rotation);
+        }
+    }
+}
---
+++ ShootingClient/Assets/#Scripts/bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, GameManager.instance.bDestoryTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(GameManager.instance.bSpeed * Time.deltaTime, 0, 0);
+
+    }
+}
